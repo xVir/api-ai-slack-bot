c05453eb699eec58b6b5b246d0f2804cb0d8aa21
Danil Skachkov <danil.skachkov@speaktoit.com>
2015-12-30 17:33:10 +0600

    Using https://github.com/howdyai/botkit

--- index.js
+++ index.js
@@ -7,140 +7,62 @@
 
 'use strict';
 
-var SlackBot = require('slackbots');
+var Botkit = require('botkit');
+
 var apiai = require('apiai');
 var uuid = require('node-uuid');
 var argv = require('minimist')(process.argv.slice(2));
 
 var apiAiService = apiai(argv.accesskey, argv.subscriptionkey);
 
-var BOT_NAME = "SlackNanny";
-var botSlackId;
-
 var sessionIds = {};
 
-// create a bot
-var bot = new SlackBot({
-    token: argv.slackkey, // Add a bot https://my.slack.com/services/new/bot and put the token
-    name: BOT_NAME
+var controller = Botkit.slackbot({
+    debug: false
+    //include "log: false" to disable logging
 });
 
+var bot = controller.spawn({
+             token: argv.slackkey
+          }).startRTM();
 
-function isDirect(messageData) {
-    if (messageData.channel) {
-        return messageData.channel.indexOf("D") == 0;
-    }
-    return false;
-}
+controller.hears(['.*'],['direct_message','direct_mention','mention', 'ambient'], function(bot,message) {
 
-function answerInDirect(messageData) {
-    var requestText = messageData.text;
-    if (requestText) {
-        var channel = messageData.channel;
+    console.log(message.text);
+    //console.log(message);
 
-        if (!(channel in sessionIds)) {
-            sessionIds[channel] = uuid.v1();
-        }
-
-        var request = apiAiService.textRequest(requestText, {sessionId: sessionIds[channel]});
-
-        request.on('response', function (response) {
-            console.log(response);
-
-            var responseText = response.result.fulfillment.speech;
-            if (responseText) {
-                bot.postMessage(channel, responseText, {}).then(
-                    function (result) {
-                        console.log("success: " + result);
-                    }
-                ).fail(
-                    function (result) {
-                        console.log("fail: " + result);
-                    }
-                );
-            }
-
-        });
-
-        request.on('error', function (error) {
-            console.log(error);
-        });
-
-        request.end();
-
-    }
-}
-
-function answerInChannel(messageData) {
-    var requestText = messageData.text;
-    var channel = messageData.channel;
-
-    if (!(channel in sessionIds)) {
-        sessionIds[channel] = uuid.v1();
-    }
-
-    var request = apiAiService.textRequest(requestText, {sessionId: sessionIds[channel]});
-    request.on('response', function (response) {
-        console.log(response);
-
-        var responseText = response.result.fulfillment.speech;
-        if (responseText) {
-            bot.postMessage(channel, responseText, {}).then(
-                function (result) {
-                    console.log("success: " + result);
-                }
-            ).fail(
-                function (result) {
-                    console.log("fail: " + result);
-                }
-            );
-        }
-    });
-
-    request.on('error', function (error) {
-        console.log(error);
-    });
-
-    request.end();
-
-
-}
-
-bot.on('start', function () {
-
-    // more information about additional params https://api.slack.com/methods/chat.postMessage
-    var params = {};
-
-    bot.getUser(BOT_NAME).then(function (data) {
-        console.log(data);
-
-        botSlackId = data.id;
-    });
-
-    // define channel, where bot exist. You can adjust it there https://my.slack.com/services
-    bot.postMessageToChannel('test-bot', 'Hello from API.AI!', params, function (result) {
-    });
-});
-
-bot.on('message', function (data) {
-    // all ingoing events https://api.slack.com/rtm
-
-    if (data.type == "message") {
-        if (data.username == BOT_NAME) {
+    if (message.type == "message") {
+        if (message.user == bot.identity.id) {
             // message from bot can be skipped
         }
         else {
             // on direct messages we should answer always
-            if (isDirect(data)) {
-                answerInDirect(data);
-            }
-            else {
-                answerInChannel(data);
+
+            var requestText = message.text;
+            var channel = message.channel;
+
+            if (!(channel in sessionIds)) {
+                sessionIds[channel] = uuid.v1();
             }
 
-            console.log(data);
+            var request = apiAiService.textRequest(requestText, { sessionId: sessionIds[channel] });
+
+            request.on('response', function (response) {
+                console.log(response.status);
+
+                if (response.result) {
+                    var responseText = response.result.fulfillment.speech;
+                    if (responseText) {
+                        bot.reply(message, responseText);
+                    }
+                }
+            });
+
+            request.on('error', function (error) {
+                console.log(error);
+            });
+
+            request.end();
         }
-
     }
-
 });
